005fcb591855b3be6577dca31f53f9398221884e
Bartosz Majdan <majdanbartek@gmail.com>
2018-08-19 01:20:27 +0200

    Update settings

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -9,7 +9,7 @@ const _SETTINGS = {
     cookie_gold_click_chance: 0.001,
     cookie_gold_click_value: 1000,
     cookie_gold_click_counter: 0,
-    cookie_gold_time_chance: 0.001,
+    cookie_gold_time_chance: 0.0008,
     cookie_gold_time_value: 600,
     cookie_gold_time_counter: 0,
     cookie_gold_time_duration: 10,
